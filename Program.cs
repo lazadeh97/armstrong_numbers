23e4306f6aefb214cb2877c001e67ce4b7dc621d
lazadeh97 <126696851+lazadeh97@users.noreply.github.com>
2023-03-16 16:30:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace armstrong_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number, digit, temp = 0;
+            decimal sum = 0;
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                number = i;
+                temp = number;
+                do
+                {
+                    digit = temp % 10;
+                    temp = Math.Floor(temp / 10);
+                    sum = sum + (digit * digit * digit* digit);
+
+                } while (temp>0);
+
+                if (sum == number)
+                {                   
+                    Console.WriteLine(number + " - " + "This number is armstrong!");
+                }
+ 
+                sum = 0;
+                number = 0;
+                temp = 0;
+            }
+        }
+    }
+}
